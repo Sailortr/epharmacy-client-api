66c90e0fe134850455ca49ff55b2c3569317ae58
SAILOR <muhammetuzunoglu@gmail.com>
2026-01-01 17:33:02 +0300

    app cors update

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.set('trust proxy', isTest ? false : 1);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors({ origin: env.corsOrigin, credentials: true }));
+app.use(cors());
 app.use(compression());
 if (!isTest) {
   app.use(morgan(env.isProd ? 'combined' : 'dev'));
